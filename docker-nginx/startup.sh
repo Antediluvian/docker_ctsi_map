5359df78e555f5ad60e5cdbe29338edc24e5b850
翠翠 <petercui@gmail.com>
2015-03-19 10:40:21 +0800

    make docker for nginx working properly.

--- docker-nginx/startup.sh
+++ docker-nginx/startup.sh
@@ -2,7 +2,9 @@
 
 docker run --privileged=true --name="ngx_01"\
  -d -e "TZ=Asia/Shanghai" -p 80:80 -p 443:443\
- -v $(pwd)/etc/site-enabled:/home/nginx/etc/site-enabled\
+ --link tc_8080:tomcat_test\
+ -v $(pwd)/etc/sites-enabled:/home/nginx/etc/sites-enabled\
+ -v $(pwd)/etc/cert:/home/nginx/etc/cert\
  -v $(pwd)/etc/nginx.conf:/home/nginx/etc/nginx.conf\
  -v $(pwd)/etc/conf.d:/home/nginx/etc/conf.d\
  -v $(pwd)/supervisord.conf:/home/supervisord.conf ctsi/nginx
