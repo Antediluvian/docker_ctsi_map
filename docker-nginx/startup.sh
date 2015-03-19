860ab7df4c86f160d72632ae9b7223f643f0eb5a
翠翠 <petercui@gmail.com>
2015-03-19 09:02:09 +0800

    docker for nginx added.

--- docker-nginx/startup.sh
+++ docker-nginx/startup.sh
@@ -2,5 +2,7 @@
 
 docker run --privileged=true --name="ngx_01"\
  -d -e "TZ=Asia/Shanghai" -p 80:80 -p 443:443\
- -v $(pwd)/site-enabled:/home/site-enabled\
+ -v $(pwd)/etc/site-enabled:/home/nginx/etc/site-enabled\
+ -v $(pwd)/etc/nginx.conf:/home/nginx/etc/nginx.conf\
+ -v $(pwd)/etc/conf.d:/home/nginx/etc/conf.d\
  -v $(pwd)/supervisord.conf:/home/supervisord.conf ctsi/nginx
